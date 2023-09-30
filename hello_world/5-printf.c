1be45cca9b8ac4bbdcf97466dd54f3a782fbc3e1
Chyanna <oakes.chy@gmail.com>
2023-09-30 16:34:17 -0700

    made 5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of the program
+ *
+ * printf - prints the line needed
+ *
+ * Return: upon no errors, returns a 0.
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
