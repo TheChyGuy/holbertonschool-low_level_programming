425b49f50cc0b0d3ea1af6066b4e0d62bf181b0a
Chyanna <oakes.chy@gmail.com>
2023-09-30 14:10:33 -0700

    edit 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,9 @@
-#include <stdio.h> 
+#include <stdio.h>
 /*Code to print a specified line*/
 
 /**
  * main - entry point of the program
- * 
+ *
  * eturn: returns a 0 upon request
  */
 int main(void)
