46d671b6d4fb9a4f2f2f9e26ca76df5a642fb92f
Chyanna <oakes.chy@gmail.com>
2023-09-30 13:16:32 -0700

    made 4

---
+++ hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h> 
+/*Code to print a specified line*/
+
+/**
+ * main - entry point of the program
+ * 
+ * eturn: returns a 0 upon request
+ */
+int main(void)
+{
+	puts("\"Programming is like a multilingual puzzle");
+	return (0);
+} 
