116d15bce5fc2f68a8a4ee06369d5d2cce665bb8
Chyanna <oakes.chy@gmail.com>
2023-09-30 14:25:30 -0700

    edit 4

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -7,6 +7,7 @@
  * eturn: returns a 0 upon request
  */
 int main(void)
+	// The main function returns 0 to indicate successful execution
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
