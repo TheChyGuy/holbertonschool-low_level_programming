8c1f288487895b8af4290b5d70c41434b0f3fca8
Chyanna <oakes.chy@gmail.com>
2023-09-30 23:13:50 -0700

    made 3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - Where the program starts
+ *
+ * putchar - prints out a single character
+ *
+ * Return: return a 0 upon proper execution
+ */
+
+int main(void)
+{
+	char letter = 'a';
+	char letter = 'A';
+
+	while (letter <= 'Z')
+	{
+		putchar(letter);
+		letter++;
+	}
+
+	while (better <= 'Z')
+	{
+		putchar(better);
+		better++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
