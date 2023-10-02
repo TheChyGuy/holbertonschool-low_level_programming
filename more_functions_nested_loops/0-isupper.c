6df52aaa0d7cc668141669370aad8051d5bbbf0e
Chyanna <oakes.chy@gmail.com>
2023-10-02 13:56:19 -0700

    edit 0

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,20 +1,2 @@
 #include "main.h"
-#include <ctype.h>
 
-/**
- * _isupper - program start
- *
- * Return: return a 1 if uppercase, else a 0
- *
- * @c: the number representing the character to be tested
- *
- * Description: returns numbers
- */
-
-int _isupper(int c)
-{
-	if (c > 64 && c < 91)
-		return (1);
-	else
-		return (0);
-}
