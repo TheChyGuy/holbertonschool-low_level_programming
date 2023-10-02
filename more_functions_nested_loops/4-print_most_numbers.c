607bd5969537664bab7208b56271310c732364e7
Chyanna <oakes.chy@gmail.com>
2023-10-02 15:22:04 -0700

    made 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints almost all numbers from the selected set of numbers. 
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	i = '0';
+
+	while (i <= '9')
+	{
+		if (i != '2' && i != '4')
+		{
+			_putchar(i);
+		}
+		i++;
+	}
+	_putchar('\n');
+}
