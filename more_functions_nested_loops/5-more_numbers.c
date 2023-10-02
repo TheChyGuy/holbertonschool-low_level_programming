27410cb5e40f9a092f33cce083e3254ccf4c2c4d
Chyanna <oakes.chy@gmail.com>
2023-10-02 15:51:01 -0700

    made 5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints more numbers.
+ */
+
+void more_numbers(void)
+{
+	int x;
+	int y;
+	char c[] = "01234567891011121314";
+
+	for (y = 0; y <= 19; y++)
+	{
+		for (x = 0; x <= 19; x++)
+		{
+			putchar(c[x]);
+		}
+		putchar('\n');
+	}
+}
