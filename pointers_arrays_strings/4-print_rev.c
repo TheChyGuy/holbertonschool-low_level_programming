63f469ca6c74c2d6162825b6b74a5d8f3b394adb
Chyanna <oakes.chy@gmail.com>
2023-10-02 20:22:42 -0700

    made 4

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+#include "2-strlen.c"
+
+/**
+ * print_rev - function
+ *
+ * @s: string
+ */
+
+void print_rev(char *s)
+{
+	int len = _strlen(s);
+
+	for (len = len - 1; len >= 0; len--)
+	{
+		putchar(s[len]);
+	}
+	putchar('\n');
+}
