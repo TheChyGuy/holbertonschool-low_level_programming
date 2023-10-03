078f404e4fca68fc80c964d56c1b8c972f70fc29
Chyanna <oakes.chy@gmail.com>
2023-10-02 18:36:12 -0700

    made 9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - enter point
+ *
+ * Return: returns 0 upon success.
+ */
+
+int main(void)
+{
+
+	int num;
+
+	num = 1;
+
+	while (num <= 100)
+	{
+		if (num % 3 == 0 && num % 5 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (num % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (num % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d, num);
+		}
+		if (num < 100)
+		{
+			printf(" ");
+		}
+		num++;
+	}
+	putchar('\n');
+	return (0);
+}
