b0c29abdcf3c08f475f6d171317449f3eb2d8ff8
TheChyGuy <144152721+TheChyGuy@users.noreply.github.com>
2023-10-20 00:43:19 -0500

    made 0-strcat.c

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcat - Concatinates the strings.
+ *
+ * @dest: start of string.
+ * @src: end of the string.
+ * Return: return *dest_start
+ */
+
+char *_strcat(char *dest, char *src)
+{
+    char *dest_start = dest;
+
+    while (*dest != '\0')
+            dest++;
+    while (*src != '\0')
+            *dest++ = *src++;
+    *dest = '\0';
+    return (dest_start);
+}
